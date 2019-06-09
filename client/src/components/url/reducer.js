b4a0ef6dddffbbe7b917ea57441d98cdcfc389fb
Tyson Acker <tyson@devmode.com>
2019-06-08 21:58:19 -0500

    Fix reducer state overwrites (assign)

--- client/src/components/url/reducer.js
+++ client/src/components/url/reducer.js
@@ -1,3 +1,4 @@
+import assign from 'lodash.assign';
 import * as types from './actionTypes';
 
 const initialState = {
@@ -8,29 +9,29 @@ const initialState = {
 export default function url(state = initialState, action) {
   switch (action.type) {
     case types.ADD_URL_SUCCESS:
-      return {
+      return assign({}, state, {
         errorMessage: null,
         records: [
           action.record,
           ...state.records
         ]
-      };
+      });
 
     case types.ADD_URL_FAILURE:
-      return {
+      return assign({}, state, {
         errorMessage: action.error.message
-      };
+      });
 
     case types.FETCH_URLS_SUCCESS:
-      return {
+      return assign({}, state, {
         errorMessage: null,
         records: action.records
-      };
+      });
 
     case types.FETCH_URLS_FAILURE:
-      return {
+      return assign({}, state, {
         errorMessage: action.error.message
-      };
+      });
 
     default:
       return state;
